f90be179952b17630b08cbf17235119aec9d9a76
philippschmalen <philippschmalen@gmail.com>
2022-01-26 09:24:29 +0100

    feat: docs, project flow, explanation in app

--- README.md
+++ README.md
@@ -1,16 +1,52 @@
-# scandal-detector
-Detect scandals for a given entity, such as firms or persons.
+# Scandal detector
+
+Detect scandals for any entity, such as public firms or celebrities.
+The algorithm only requires sufficient search interest for a keyword and its associated scandals.
+
+## Project brief
+
+![](img/data-flow.png)
+
+---
+## Objective
+
+Detect scandals for publicly known firms within the last 5 years.
+
+## Key results
+
+1. Show overall number of scandals
+2. Display scandals in timeline
+3. Provide links to Google for further investigation
+
+## Data
+
+The project builds on [Google Trends](https://trends.google.com/trends/?geo=GLOBAL) data. It covers search interest for a keyword over the last five years.
+
+The data pipeline includes the following steps:
+
+1. a user inputs a keyword to which we add "scandal"
+2. the search interest is fetched from Google Trends with [pytrends](https://github.com/GeneralMills/pytrends).
+3. Process the data with pandas
+4. train a time-series model with [Facebook Prophet](https://facebook.github.io/prophet/).
+5. Identify outliers from the predicted search interest. These are the scandals.
+5. Visualize timeline with plotly
+6. List links to Google in the period of the scandal
+
 
 
 ---
 
-## BACKLOG
+## Extensions
 
 - [ ] check that detected scandals do not overlap within a period of `date_uppderbound_days`
+- [ ] install pre-commit
 - [ ] create `config.yaml`
 - [ ] load config with `hydra`
 
+
 ## Color codings
+
+Using color codings from http://towardssustainablefinance.com/.
 
 color_discrete_sequence=["#4d886d", "#f3dab9", "#9bcab8", "#829fa5", "#cfaea5"]
 dark font color: #545454
--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import logging
 import streamlit as st
-from prophet import Prophet
-import pandas as pd
 from src.utils_data import get_google_trends_firm_scandal
 from src.utils_data import get_df_pred
 from src.utils_data import get_list_of_scandal_links
@@ -17,7 +15,6 @@ logging.basicConfig(
 detector_sensitivity = 0.99
 date_uppderbound_days = 30
 keep_topn_scandals = 3
-
 dict_geo = {
     "DE": {"domain": "de", "keyword": "Skandal", "gtrends": "DE"},
     "US": {"domain": "com", "keyword": "scandal", "gtrends": "US"},
@@ -26,37 +23,24 @@ dict_geo = {
 }
 
 
-# SELECTIONS
 st.title("Scandal Detector")
 st.subheader("Search major scandals for publicly known companies")
 
+# SELECTIONS
 keyword_userinput = st.text_input(label="Enter a firm name:", value="deutsche bank")
 select_geo = st.selectbox(label="Select a country", options=dict_geo)
 
-# ------------------------------------------
-# TEST DATA
-# keyword_userinput = "deutsche bank"
-# filepath_mockdata = "data/test/mockdata_deutschebank.csv"
-
-# if st.sidebar.button("Get fresh Google Trends"):
-#     df_raw = get_google_trends_firm_scandal(keyword_userinput)
-#     df = df_raw.copy()
-#     st.info(f"Got google trends for {keyword_userinput} with {len(df)} rows")
-# else:
-#     df = pd.read_csv(filepath_mockdata, parse_dates=["date", "ds"])
-#     st.info("Loaded mock data")
-# ------------------------------------------
-
-
-# MAIN
+# DATA PIPELINE
 if st.button("Detect scandals"):
     with st.spinner("Fetching data..."):
+        # fetch google trends
         df = get_google_trends_firm_scandal(
             keyword_userinput, dict_domain_keyword=dict_geo[select_geo]
         )
 
         if df is None:
             st.info(f"No public scandals found for {keyword_userinput}.")
+
         else:
             df_pred = get_df_pred(
                 df,
@@ -68,17 +52,31 @@ if st.button("Detect scandals"):
             st.info(
                 f"Found {len(df_pred.query('outlier == 1'))} major public scandal(s) within the last 5 years."
             )
+
+            # PLOT
             st.subheader("Timeline on scandals over last 5 years")
             fig = plotly_timeline(df_pred.copy(), keyword_userinput, keep_topn_scandals)
             st.plotly_chart(fig)
 
             st.subheader("Scandals sorted by severity")
 
-            # write links as list
+            # link list
             st.write(
                 get_list_of_scandal_links(df_pred).to_html(escape=False, index=False),
                 unsafe_allow_html=True,
             )
+
+
+with st.expander("See how it works"):
+    """
+    The scandal detector uses search interest from Google trends and applies a time-series model to identify public scandals.
+    """
+    st.image("img/data-flow.png")
+    """
+    You can access the source code on https://github.com/philippschmalen/scandal-detector. Feel free to contribute!
+
+    If you have made it so far, I would love ot hear from you. Drop me a line on [linkedin](https://www.linkedin.com/in/philippschmalen/).
+    """
 
 st.write("---")
 st.image("img/tsf_logo.png")
